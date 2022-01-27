59c83810cc3a02c0208510e705826622625c60b5
bryanagamk <bryan@kyodo-i.com>
2022-01-27 14:25:16 +0700

    update validation items

--- app/api/items/router.js
+++ app/api/items/router.js
@@ -1,13 +1,13 @@
 var express = require('express');
+const { validateOne, validateCreate, validateUpdate, validateMove } = require('./validation');
 var router = express.Router();
-const { getAll, create, getOne, update, destroy, move } = require('./controller')
-
+const { getAll, create, getOne, update, destroy, move } = require('./controller');
 /* GET home page. */
 // router.get('/items', getAll);
-router.get('/items/:id', getOne);
-router.post('/items', create);
-router.put('/items/:id', update);
-router.put('/items/:id/move', move);
-router.delete('/items/:id', destroy);
+router.get('/items/:id', validateOne, getOne);
+router.post('/items', validateCreate, create);
+router.put('/items/:id', validateUpdate, update);
+router.put('/items/:id/move', validateMove, move);
+router.delete('/items/:id', validateUpdate, destroy);
 
 module.exports = router;
